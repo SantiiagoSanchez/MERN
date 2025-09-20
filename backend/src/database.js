960cf49795a0027d89d9fab1f7822237e59341c1
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-09-20 02:06:28 -0300

    Conectando la bd

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,1 +1,17 @@
 //Aca vamos a definir la cadena de conexion a la base de datos
+
+const mongoose = require('mongoose')
+
+//Cadena de conexion
+//Si existe una variable de entorno MONGODB_URI la usamos, sino usamos localhost
+const URI = process.env.MONGODB_URI
+            ? process.env.MONGODB_URI
+            : 'mongodb://localhost/dbtest'
+
+mongoose.connect(URI)
+
+const conection = mongoose.connection;
+
+conection.once('open', ()=>{
+    console.log('La base de datos a sido conectada: ', URI)
+}) 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 //Aca vamos a crear el arranque de nuestro servidor
 
+require('dotenv').config() //Esto es para que las variables de entorno funcionen
+
 const app = require('./app')
+
+require('./database')
 
 //Esto es para ejecutar el servidor
 async function main() {
