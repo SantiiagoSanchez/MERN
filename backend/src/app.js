21cf1ad9437bc08a807a84473186f2dc38e23052
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-09-19 21:49:48 -0300

    Configuracion del servidor

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,13 @@
 //Este archivo sirve para configurar el servidor con las rutas y más cosas
+
+const express = require('express')
+const cors = require('cors')
+const app = express();
+
+//Configuracion
+//Esto es para decirle que nos guarde una variable llamada port, como segundo parametro le decimos el puerto para que lo ejecute, le pasamos la variable de entorno "PORT", si no tiene una variable "PORT" que ejecute el puerto 4000
+app.set('port', process.env.PORT || 4000)
+
+
+//Exportamos el app para que en otro lugares del proyecto lo podamos utilizar
+module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,12 @@
 //Aca vamos a crear el arranque de nuestro servidor
+
+const app = require('./app')
+
+//Esto es para ejecutar el servidor
+async function main() {
+    await app.listen(app.get('port'))
+    console.log('El servidor se esta ejecutando en el puerto: ', app.get('port'));
+    
+}
+
+main();
