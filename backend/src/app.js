44742b9281ce0a2f6281f037e122fadcbba74625
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-09-20 02:18:16 -0300

    Configuraciones extras en app

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,18 @@ const app = express();
 //Esto es para decirle que nos guarde una variable llamada port, como segundo parametro le decimos el puerto para que lo ejecute, le pasamos la variable de entorno "PORT", si no tiene una variable "PORT" que ejecute el puerto 4000
 app.set('port', process.env.PORT || 4000)
 
+//Middlewares. Funcionalidades para poder decirle al servidor que antes de que ejecute las rutas, primero pase por los middlewares. De esta manera se va a validar las peticiones o configuracion, una vez todo ok, pasa a las rutas
+
+app.use(cors()) //Nos permite hacer peticiones desde un servidor distinto al del servidor de nuestro backend
+
+app.use(express.json()) //Esto es para que cuando hagamos una peticion http, nos devuelve o enviamos json.
+
+//Rutas
+
+app.get('/', (req, res)=>{
+    res.send("Bienvenido a mi API Rest full");
+})
+
 
 //Exportamos el app para que en otro lugares del proyecto lo podamos utilizar
 module.exports = app;
