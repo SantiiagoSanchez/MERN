c0c096f078552efda6140dc9435fd76a8ba5f7a1
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-09-24 11:47:46 -0300

    POST funcionando

--- backend/src/models/Usuario.js
+++ backend/src/models/Usuario.js
@@ -5,7 +5,7 @@ const usuarioSchema = new Schema({
     apellido: String,
     edad: Number,
     telefono: Number,
-    correo: Number
+    correo: String
 },
 {
     timestamps: true //Esto es para almacenar automaticamente cuando se creo el documento, y cuando se actualizo el documento
--- frontend/src/components/CrearUsuarios.js
+++ frontend/src/components/CrearUsuarios.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 
 const CrearUsuarios = () => {
+
+    const valorInicial = {
+        nombre: '',
+        apellido: '',
+        edad: 18,
+        telefono:0,
+        correo: ''
+
+    }
+
+    const [usuario, setUsuario] = useState(valorInicial) //Nos permite crear una variable de estado, para poder almacenar informacion dentro de la variable.
+    
+    const capturarDatos = (e) =>{
+        const {name, value} = e.target
+        setUsuario({...usuario, [name]: value})
+    }
+
+
+    const guardarDatos = async (e) =>{
+        e.preventDefault(); //Esto es para que no se recargue la pagina una vez guardar usuario
+
+        //HttpPost
+
+        const newUser = {
+            nombre: usuario.nombre,
+            apellido: usuario.apellido,
+            edad: usuario.edad,
+            telefono: usuario.telefono,
+            correo: usuario.correo
+        }
+
+        await axios.post('http://localhost:4000/api/usuarios', newUser)
+
+        setUsuario({...valorInicial}) //Esto es para limpiar los inputs
+        
+    }
+
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
         <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
             <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
             Crear Usuario
             </h2>
-            <form>
+            <form onSubmit={guardarDatos}>
             <div className="mb-4">
                 <label className="block text-gray-700 mb-1">Nombre:</label>
                 <input
@@ -15,6 +54,9 @@ const CrearUsuarios = () => {
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Ingrese su nombre"
                 required
+                name="nombre"
+                value={usuario.nombre}
+                onChange={capturarDatos} //Evento para cuando escribamos en la caja de texto, esto captura automaticamente el valor y lo guardamos en la variable de estado
                 />
             </div>
             <div className="mb-4">
@@ -24,6 +66,9 @@ const CrearUsuarios = () => {
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Ingrese su apellido"
                 required
+                name="apellido"
+                value={usuario.apellido}
+                onChange={capturarDatos}
                 />
             </div>
             <div className="mb-4">
@@ -33,6 +78,9 @@ const CrearUsuarios = () => {
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Ingrese su edad"
                 required
+                name="edad"
+                value={usuario.edad}
+                onChange={capturarDatos}
                 />
             </div>
             <div className="mb-4">
@@ -42,6 +90,9 @@ const CrearUsuarios = () => {
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Ingrese su teléfono"
                 required
+                name="telefono"
+                value={usuario.telefono}
+                onChange={capturarDatos}
                 />
             </div>
             <div className="mb-6">
@@ -51,6 +102,9 @@ const CrearUsuarios = () => {
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="example@example.com"
                 required
+                name="correo"
+                value={usuario.correo}
+                onChange={capturarDatos}
                 />
             </div>
             <button
